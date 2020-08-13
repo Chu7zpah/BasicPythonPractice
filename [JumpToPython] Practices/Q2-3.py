1e6a94a8c1bb904a4990d28ae986afd016477e85
Chu7zpah <chu7zpah@gmail.com>
2020-08-13 16:23:22 +0900

    #Q2-3 Social Serial Number Slicing Program Updating Input format

--- [JumpToPython] Practices/Q2-3.py
+++ [JumpToPython] Practices/Q2-3.py
@@ -1,6 +1,7 @@
 # Social Serial Number Slicing Program
 # Slicing to YYMMDD(Front) and Others(Back)
 
+print("Input format: [123456-1234567]")
 social_serial_number = input("Enter Social Serial Number: ")
 ssn_split = social_serial_number.split("-")
 front = ssn_split[0]
