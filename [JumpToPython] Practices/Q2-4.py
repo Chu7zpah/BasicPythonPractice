f2f569ddb268f110137e683a279f0ef6fd779aca
Chu7zpah <chu7zpah@gmail.com>
2020-08-13 16:25:59 +0900

    #Q2-4 Social Serial Number Gender Extractor

---
+++ [JumpToPython] Practices/Q2-4.py
@@ -0,0 +1,6 @@
+# Social Serial Number Gender Extractor
+
+print("Input format: [123456-1234567]")
+social_serial_number = input("Enter Social Serial Number:")
+gender_number = social_serial_number[7]
+print("Gender Number: {}".format(gender_number))
