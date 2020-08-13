178b62e1d00c77f5c02f09beb13bad4979892fc9
Chu7zpah <chu7zpah@gmail.com>
2020-08-13 16:12:18 +0900

    #Q2-2 Odd/Even Number Sorting Program

---
+++ [JumpToPython] Practices/Q2-2.py
@@ -0,0 +1,7 @@
+# Odd/Even Number Sorting
+
+num = int(input("Enter an integer: "))
+if num % 2 == 0:
+    print("{} is an EVEN number".format(num))
+else:
+    print("{} is an ODD number".format(num))
