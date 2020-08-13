0f24ec0e9774f8ac57dd715320000b19d24ee451
Chu7zpah <chu7zpah@gmail.com>
2020-08-13 16:46:30 +0900

    #Q2-8 Adding element in Tuple using '+' 1 element Tuple = (4,)

---
+++ [JumpToPython] Practices/Q2-8.py
@@ -0,0 +1,8 @@
+# Adding element in Tuple
+# Using '+'
+
+original_tuple = (1, 2, 3)
+new_tuple = original_tuple + (4,)
+
+print("Original: {}".format(original_tuple))
+print("New: {}".format(new_tuple))
