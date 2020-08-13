de81a65886f803ab45c9e2f67510c65eceb4797e
Chu7zpah <chu7zpah@gmail.com>
2020-08-13 16:35:16 +0900

    #Q2-6 List Element Ordering Program

---
+++ [JumpToPython] Practices/Q2-6.py
@@ -0,0 +1,6 @@
+# List Element Ordering Program
+
+sorting_list = [1, 3, 5, 4, 2]
+print("Original List: {}".format(sorting_list))
+sorting_list.sort(reverse=True)
+print("Sorted List: {}".format(sorting_list))
