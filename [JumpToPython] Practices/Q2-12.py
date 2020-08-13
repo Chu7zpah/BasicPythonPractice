e7752bebc980d97d9980f746e8ee14260c76bc58
Chu7zpah <chu7zpah@gmail.com>
2020-08-13 17:11:52 +0900

    #Q2-12 (Important!) Python's Variable to Object Relationship

---
+++ [JumpToPython] Practices/Q2-12.py
@@ -0,0 +1,13 @@
+# Python's Variable to Object Relationship
+
+# Making an object [1, 2, 3] in Memory
+# Variable a and b points object [1, 2, 3]
+a = b = [1, 2, 3]
+
+# Change object [1, 2, 3] to [1, 4, 3]
+a[1] = 4
+
+# print(b) will return [1, 4, 3]
+print(b)
+
+# Python Variable is similar to Pointer Variable in C!
