436acdbb3e1493001abf7c0677c64c54acfd90dd
Chu7zpah <chu7zpah@gmail.com>
2020-08-13 16:40:56 +0900

    #Q2-7 From List to String Program using Join Function

---
+++ [JumpToPython] Practices/Q2-7.py
@@ -0,0 +1,6 @@
+# From List to String Program
+# Using Join Function
+
+original_list = ['Life', 'is', 'too', 'short']
+joined_string = " ".join(original_list)
+print("String made by List: " + joined_string)
