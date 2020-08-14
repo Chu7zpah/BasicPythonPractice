c072cf1d289e9c95fc0883e32045edaa0f491815
Chu7zpah <chu7zpah@gmail.com>
2020-08-14 15:47:52 +0900

    #Q3-1 If, Elif, Else Practice

---
+++ [JumpToPython] Practices/3. Control Statement/Q3-1.py
@@ -0,0 +1,14 @@
+# if, elif, else Practice
+
+a = "Life is too short, you need python"
+
+if "wife" in a:     # False
+    print("wife")
+elif "python" in a and "you" not in a:  # True & False = False
+    print("python")
+elif "shirt" not in a:  # True
+    print("shirt")
+elif "need" in a:   # False
+    print("need")
+else:               # False
+    print("none")
