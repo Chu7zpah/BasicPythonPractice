38e96852a4ef1218f25de6601098085a83743f0d
Chu7zpah <chu7zpah@gmail.com>
2020-08-14 16:12:37 +0900

    #Q3-2 While Statement: Sum Multiples of 3

---
+++ [JumpToPython] Practices/3. Control Statement/Q3-2.py
@@ -0,0 +1,13 @@
+# Sum Multiples of 3 in range of 1 to 1,000
+# Using While Statement
+
+num = sum = 0
+
+while num <= 1000:
+    num = num + 1
+    if num % 3 == 0:    # Multiples of 3
+        sum += num
+    else:
+        continue
+
+print("Result:", sum)
