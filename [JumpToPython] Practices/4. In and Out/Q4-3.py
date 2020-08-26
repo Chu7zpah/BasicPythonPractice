223d4528677f8d0521095a2f22a705518617f180
Chu7zpah <chu7zpah@gmail.com>
2020-08-26 11:47:02 +0900

    # Q4-3
    + Operator Overloading

---
+++ [JumpToPython] Practices/4. In and Out/Q4-3.py
@@ -0,0 +1,16 @@
+# + Operator Overloading (Str = concatenation)
+'''
+input1 = input("Enter 1st number: ")
+input2 = input("Enter 2nd number: ")
+
+# total = (input1input2)
+total = input1 + input2
+print("Sum of two is {0}".format(total))
+'''
+
+
+input1 = input("Enter 1st number: ")
+input2 = input("Enter 2nd number: ")
+
+total = int(input1) + int(input2)
+print("Sum of two is {0}".format(total))
