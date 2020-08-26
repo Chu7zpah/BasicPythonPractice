c9ea480f673253ebcd12c04886ab5dcb807d8780
Chu7zpah <chu7zpah@gmail.com>
2020-08-26 11:52:46 +0900

    # Q4-4 Which one's output is different?

---
+++ [JumpToPython] Practices/4. In and Out/Q4-4.py
@@ -0,0 +1,8 @@
+# Which one's output is different?
+# 3rd one: 'you need python'
+# others: 'youneedpython'
+
+print("you" "need" "python")
+print("you"+"need"+"python")
+print("you", "need", "python")
+print("".join(["you", "need", "python"]))
