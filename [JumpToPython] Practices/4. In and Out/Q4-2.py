2572435ecb9cd30fb075ae1356d1acb8eecbd7e3
Chu7zpah <chu7zpah@gmail.com>
2020-08-26 11:39:12 +0900

    # Q4-2
    Using * arguments added

--- [JumpToPython] Practices/4. In and Out/Q4-2.py
+++ [JumpToPython] Practices/4. In and Out/Q4-2.py
@@ -1,7 +1,7 @@
 # Average Calculator
 # No Input details(# of inputs)
 # Using Function
-
+'''
 def average_calculate(nums):
     sum = 0
     for i in nums:
@@ -13,3 +13,18 @@ def average_calculate(nums):
 num_lists = input("Enter numbers to calculate(distinguished by ' '): ").split()
 average = average_calculate(num_lists)
 print(average)
+
+'''
+
+# Using * argument
+def average_calculate(*nums):
+    sum = 0
+    for i in nums:
+        sum += i
+
+    average = sum/len(nums)
+    return average
+
+
+result = average_calculate(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(result)
